0c2aadfbbf14a12760b8d1cabb4b4232a00bc4a2
J.D. Bean <jonathandbean@gmail.com>
2018-02-25 18:53:55 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,5 +7,5 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by { |phrase| ESPERANTO_ALPHABET.index(phrase) }
+  arr.sort_by { |phrase| ESPERANTO_ALPHABET.index(phrase[0]) }
 end
