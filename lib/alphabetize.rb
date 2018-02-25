7319f28d5407ee9ec4f78577598ae788157ba209
J.D. Bean <jonathandbean@gmail.com>
2018-02-25 19:00:00 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -12,7 +12,7 @@ def alphabetize(arr)
   arr.sort_by do |phrase|
     phrase.split("").map do |letter|
       ESPERANTO_ALPHABET.index(letter)
-      binding.pry
+      # binding.pry
     end
   end
 end
