15488933b28977258a048f985aa060621aefbd61
J.D. Bean <jonathandbean@gmail.com>
2018-02-25 18:50:31 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+# #alphabetize
+#   alphabetizes based on the first letter
+#   alphabetizes based on second letters
+#   alphabetizes based on sixth letters 
+#   alphabetizes based on second letters not in English alphabet
+
+
 def alphabetize(arr)
-  # code here
+  ESPERANTO_ALPHABET_CONST = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by { |phrase| ESPERANTO_ALPHABET_CONST.array}
 end
