d23e4c5d28920d62218cfcaf657dac83cd5115c3
J.D. Bean <jonathandbean@gmail.com>
2018-02-25 18:58:12 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -10,8 +10,8 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   arr.sort_by do |phrase|
-    phrase.split("").map do |character|
-      ESPERANTO_ALPHABET.index(character)
+    phrase.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
       binding.pry
     end
   end
